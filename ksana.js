e11189a0168db66e8da6e2ed0ffe8948c0fd0e20
tomtom89516 <abcdef830327@gmail.com>
2015-03-18 15:26:00 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,45 @@
+jsonp_handler({
+ "version": "1",
+ "build": 10,
+ "title": "to-do-list",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/tomtom89516/to-do-list/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css"
+ ],
+ "filesizes": [
+  547,
+  116,
+  652507,
+  95357,
+  277980,
+  781,
+  35607,
+  113502
+ ],
+ "filedates": [
+  "2015-03-18T05:41:05.615Z",
+  "2015-03-18T06:17:58.559Z",
+  "2015-03-18T05:41:05.896Z",
+  "2015-03-18T06:46:36.795Z",
+  "2015-03-18T05:41:05.740Z",
+  "2015-03-18T06:46:02.053Z",
+  "2015-03-18T05:41:05.584Z",
+  "2015-03-18T05:41:05.490Z"
+ ],
+ "date": "2015-03-18T06:46:37.773Z"
+})
